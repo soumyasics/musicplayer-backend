1663cfd8a27fbdb555420e7960b285ec4bc01f2b
Soumya Praveen <soumya.sics@gmail.com>
2023-12-28 15:45:35 +0530

    Skr

---
+++ Creator/CreatorSchema.js
@@ -0,0 +1,70 @@
+const mongoose = require("mongoose");
+
+const CreatorSchema=mongoose.Schema({
+    firstname: {
+        type: String,
+        required: true
+    },
+    lastname: {
+        type: String,
+        required: true
+    },
+
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+
+        dropDups: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+  
+    dob: {
+        type: Date,
+        required: true
+    },
+    gender: {
+        type: String,
+        required: true
+    },
+    street: {
+        type: String,
+        required: true
+    },
+
+    city: {
+        type: String,
+        required: true
+    },
+
+    state: {
+        type: String,
+        required: true
+    },
+
+    pincode: {
+        type: String,
+        required: true
+    },
+    mobile: {
+        type: Number,
+        required: true
+    },
+    country:{
+        type:String,
+        required:true
+    },
+    image: {
+        type: Object,
+       
+    },
+    aadhar:{
+        type: Number,
+        required: true
+    }
+
+})
+module.exports=mongoose.model('creators',CreatorSchema)
--- routes.js
+++ routes.js
@@ -1,9 +1,24 @@
 const express = require("express");
 const router = express.Router();
 const listener=require('./Listener/ListenerController')
+const creator=require('./Creator/CreatorController')
+
 // const Creator=require('./Creator')
 
 
 router.post('/listenerregister',listener.upload,listener.ListenerRegister)
-router.post('/listenerlogin',listener.ListenerLogin)
+router.post('/editListenerById/:id',listener.upload,listener.editListenerById)
+router.post('/viewListenerById/:id',listener.viewListenerById)
+router.post('/deleteListenerById/:id',listener.deleteListenerById)
+router.post('/forgotPwd',listener.forgotPwd)
+router.post('/viewListeners',listener.viewListeners)
+router.post('/listenerLogin',listener.ListenerLogin)
+
+
+router.post('/CreatorRegister',creator.upload,creator.CreatorRegister)
+router.post('/editCreatorById',creator.upload,creator.editCreatorById)
+router.post('/viewCreatorById/:id',creator.viewCreatorById)
+router.post('/deleteCreatorById/:id',creator.deleteCreatorById)
+router.post('/forgotPwdCreator',creator.forgotPwdCreator)
+router.post('/viewCreators',creator.viewCreators)
 module.exports=router
